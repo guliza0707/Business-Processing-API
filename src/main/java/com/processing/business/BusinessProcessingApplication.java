e645c8e72c394dee2b1fe707494eef416a16d080
Avazbek Niyazov <avazbe@gmail.com>
2021-11-22 19:37:25 +0500

    Initial commit

---
+++ src/main/java/com/processing/business/BusinessProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.processing.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/processing/business/BusinessProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processing.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
